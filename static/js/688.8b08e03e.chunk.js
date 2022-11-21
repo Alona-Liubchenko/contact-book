9ef911e1bc4076041a4287cc380aa5119d7e6b4f
Alona-Liubchenko <98355708+Alona-Liubchenko@users.noreply.github.com>
2022-11-21 05:32:52 +0000

    Deploying to gh-pages from @ Alona-Liubchenko/goit-react-hw-08-phonebook@9544abf5a044a2e6f167794fb9d00871c8f9e903 🚀

---
+++ static/js/688.8b08e03e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[688],{688:function(n,e,t){t.r(e),t.d(e,{default:function(){return i}});var r,a=t(168),c=t(6444).ZP.p(r||(r=(0,a.Z)(["\n  text-transform: uppercase;\n  font-size: 52px;\n  font-weight: 800;\n  margin-top: 70px;\n  margin-left: 40px;\n  text-align: center;\n  color: #0d0d45;\n"]))),o=t(3329);function i(){return(0,o.jsx)("div",{children:(0,o.jsx)(c,{children:" Your contacts manager "})})}}}]);
+//# sourceMappingURL=688.8b08e03e.chunk.js.map
